247c80ea3562d608b4921fb283851ef95bbe91a6
c1audio <github@x21.ch>
2026-05-08 13:06:47 +0200

    better fit tutorial video iframe

--- menu/script.js
+++ menu/script.js
@@ -1,10 +1,27 @@
 import { getCookie } from "../js/cookies.js"
 
-let checkinButton = document.querySelector("#checkinButton");
-let tuneInButton = document.querySelector("#tuneInButton");
-let nickname = getCookie("nickname");
+let vimeoPlayer = document.querySelector("#vimeo-player");
 
-if(nickname!=="") {
-    checkinButton.hidden=true;
-    tuneInButton.hidden=false;
+function resizeIFrameToFitContent( iFrame ) {
+    console.log("onresize " + document.documentElement.clientWidth);
+    vimeoPlayer.width  = document.documentElement.clientWidth;
+    vimeoPlayer.height = document.documentElement.clientWidth*0.56;
 }
+
+window.addEventListener('DOMContentLoaded', function(e) {
+    let checkinButton = document.querySelector("#checkinButton");
+    let tuneInButton = document.querySelector("#tuneInButton");
+    let nickname = getCookie("nickname");
+
+    resizeIFrameToFitContent( vimeoPlayer );
+
+    if(nickname!=="") {
+        checkinButton.hidden=true;
+        tuneInButton.hidden=false;
+    }
+
+} );
+
+window.addEventListener("resize", function() {
+    resizeIFrameToFitContent( vimeoPlayer );
+});
